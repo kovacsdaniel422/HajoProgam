8407dee9a47a081daed75fa662aa77bd87a542c5
kovacsdaniel422 <kovacsdaniel422@gmail.com>
2022-05-23 14:50:27 +0200

    elso commit

---
+++ src/main/TorpedoTeszt.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+
+/**
+ *
+ * @author Szf1_A2
+ */
+public class TorpedoTeszt {
+    
+    public static void main(String[] args) {
+        
+    }
+    public String tesztLoves(int poz){
+    
+    return "";
+    }
+    
+}
